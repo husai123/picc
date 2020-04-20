e814da8b2a068e124a9a2629d1962ff6fd282a55
1962197683 <1962197683@qq.com>
2020-04-20 10:32:08 +0800

    陈文俊提交

---
+++ insurance_upload/src/main/java/org/java/shopping/vo/FileVO.java
@@ -0,0 +1,18 @@
+package org.java.shopping.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *  封装文件上传的以后的返回结果
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileVO {
+
+    private int code;//状态码： 1：成功  0：失败
+    private String msg;//文件上传成功或失败的提示信息
+    private String data;//文件上传成功后的保存路径
+}
